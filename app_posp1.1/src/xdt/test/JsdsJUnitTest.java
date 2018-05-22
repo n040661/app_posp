abc79960a2488a814ef20436e1fa58af2e3794a8
chao1023 <1815822346@qq.com>
2018-05-22 17:11:07 +0800

    180522接口调整

--- app_posp1.1/src/xdt/test/JsdsJUnitTest.java
+++ app_posp1.1/src/xdt/test/JsdsJUnitTest.java
@@ -2,9 +2,13 @@ package xdt.test;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import xdt.util.RSAUtil;
 
 public class JsdsJUnitTest extends TestCase{
 
@@ -82,6 +86,32 @@ public class JsdsJUnitTest extends TestCase{
 	public void testAdd() {
 		fail("Not yet implemented");
 	}
-    
+   public static void main(String[] args) {
+	   try {
+	   String aa="iyMScJch9P8QyqMbPBjlbnFMXfUsOm66G1zTK1vcJFLfCG46KoyXVyk0LMa35Fs9H83ZEbsdqpKc%207Ge/FRZs4zsBm7Oda4x2WnNmXE7iLDgqD8xmfq1VfGs5JOg753yG227+WaQjKtBZfGotVRHcoaKt%20yZAF6PVMQFR2VU6gBNE=";
 
+		//String baseSign = URLDecoder.decode(aa,"UTF-8");
+
+       
+		String baseSign = aa.replace(" ", "+");
+		 String bb=baseSign.replace("%20", "");
+		 //String cc="iyMScJch9P8QyqMbPBjlbnFMXfUsOm66G1zTK1vcJFLfCG46KoyXVyk0LMa35Fs9H83ZEbsdqpKc 7Ge/FRZs4zsBm7Oda4x2WnNmXE7iLDgqD8xmfq1VfGs5JOg753yG227 WaQjKtBZfGotVRHcoaKt yZAF6PVMQFR2VU6gBNE=";
+		 
+//		 System.out.println(cc.replaceAll(" ", ""));
+//		 if(bb.equals(cc.replaceAll(" ", "")))
+//		 {
+//			 System.out.println("123333333");
+//		 }
+		byte[] a = RSAUtil.verify("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCSUnSUG5I3Xh2ANLpC5xLe96WCVQG+A5iPBKPqRKBcF2OCdCtwNs8X40nyqYnVWqhkZwGiItT4+wFc04boL1Az01UJiZBLqmOumU0mxyyKCqGwFZakl3LWI4u2IBDuwyde3muXZDWtSDBH1k2BKzOHju3eeSicZu5D7SQ1Hol7AwIDAQAB",RSAUtil.base64Decode(bb));
+
+		String Str = new String(a);
+		
+		System.out.println(Str);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+}
+	
 }
