3a60e624e39584955532bfaa83d7a08677d60029
gaoliming <617614698@qq.com>
2018-07-18 13:50:04 +0800

    20180718

--- app_posp1.1/src/xdt/controller/GateWayController.java
+++ app_posp1.1/src/xdt/controller/GateWayController.java
@@ -1303,7 +1303,7 @@ public class GateWayController extends BaseAction {
 				if ("01".equals(synchNotifyParams.get("transStatus"))) {
 					if("000001110100000663".equals(request.getParameter("merchantId"))) {
 						int ii =0;
-						if("10036048439".equals(originalInfo.getPid())) {
+						if("10036048439".equals(originalInfo.getPid())||"10032051737".equals(originalInfo.getPid())||"10032057782".equals(originalInfo.getPid())) {
 							ii =gateWayService.UpdatePmsMerchantInfo(originalInfo,1.0);
 						}else {
 							ii =gateWayService.UpdatePmsMerchantInfo(originalInfo,0.5);
